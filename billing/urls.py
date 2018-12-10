17e6fa334deb8ffe2a51f858e559331aaea34cc5
Nicholas Wolff <nwolff@gmail.com>
2018-12-10 15:33:59 +0100

    fix deprecation warning

--- billing/urls.py
+++ billing/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from .views import AccountView, CreditCardViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register(r'account/credit-cards', CreditCardViewSet, base_name='billing_creditcard')
+router.register(r'account/credit-cards', CreditCardViewSet, basename='billing_creditcard')
 
 urlpatterns = router.urls + [
     url(r'^account$', AccountView.as_view(), name='billing_account'),
