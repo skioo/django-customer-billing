f97610590236ac6de4f078ed569497d372a7fb7e
Nicholas Wolff <nwolff@gmail.com>
2019-02-10 19:48:06 +0000

    no django 2.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'templates/admin/billing/*.html',
     ]},
     install_requires=[
-        'Django>=2.0',
+        'Django>=2.0,<2.2',
         'django-money',
         'django-fsm',
         'djangorestframework',
