8eeccfe4672f1ea003187dd7cda0caafe7bc29ca
Alberto Pou <albertopouquiros@gmail.com>
2020-09-30 14:16:31 +0200

    Add Account.delinquent db index

--- billing/migrations/0018_account_delinquent.py
+++ billing/migrations/0018_account_delinquent.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='account',
             name='delinquent',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(db_index=True, default=False),
         ),
     ]
