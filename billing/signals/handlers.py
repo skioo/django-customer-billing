99f5761f8d3bb1a8830dd4f5dd5feb554290102f
Alberto Pou <albertopouquiros@gmail.com>
2020-10-06 16:24:54 +0200

    Delete handler, now is controlled from django-customer-billing-datatrans

--- billing/signals/handlers.py
+++ billing/signals/handlers.py
@@ -1,10 +1,8 @@
-from billing_datatrans.signals import credit_card_registered
 from django.dispatch import receiver
 from structlog import get_logger
 
 from billing.signals import new_delinquents
 from ..models import EventLog
-from ..actions import invoices
 
 logger = get_logger()
 
@@ -24,22 +22,4 @@ def new_delinquents_handler(sender, **kwargs):
         )
         for account_id, reasons in new_delinquent_accounts_map.items()
     ])
-
-
-@receiver(credit_card_registered)
-def credit_card_registered_handler(sender, **kwargs):
-    credit_card = kwargs['credit_card']
-    account = credit_card.account
-    if account.delinquent:
-        pending_invoices = account.invoices.payable().only('pk')
-        logger.info(
-            'creditcard-registered-handler',
-            pending_invoices=pending_invoices
-        )
-        payments = []
-        for invoice in pending_invoices:
-            payment = invoices.pay_with_account_credit_cards(invoice.pk)
-            payments.append(payment)
-        if len(payments) == len(pending_invoices):
-            account.mark_as_compliant()
 
