8c578bfb1e562569eb541a2f94b4e187916fac1c
Nicholas Wolff <nwolff@gmail.com>
2020-10-13 11:29:30 +0200

    add credit_card_registered signal

--- billing/signals/__init__.py
+++ billing/signals/__init__.py
@@ -8,3 +8,5 @@ invoice_ready = Signal(providing_args=['invoice'], use_caching=True)
 delinquent_status_updated = Signal(
     providing_args=['new_delinquent_accounts_map', 'new_compliant_accounts_ids']
 )
+
+credit_card_registered = Signal(providing_args=['credit_card'])
