7bcc112a4a442a31828c9e8700601b2b8dd72808
Alberto Pou <albertopouquiros@gmail.com>
2020-10-20 09:25:51 +0200

    Delete unused import

--- billing/views.py
+++ billing/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK
 from rest_framework.viewsets import GenericViewSet
 
 from .actions.accounts import (
