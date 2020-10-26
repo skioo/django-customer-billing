a571fb598f0c40725144c8d9376a7cabd9df9f08
Alberto Pou <albertopouquiros@gmail.com>
2020-10-26 09:04:41 +0100

    Credit card save has to be performed before update account delinquent status

--- billing/admin.py
+++ billing/admin.py
@@ -189,6 +189,7 @@ class CreditCardAdmin(AppendOnlyModelAdmin):
     readonly_fields = ['created', 'modified', 'expiry_date']
 
     def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
         if 'expiry_month' in form.changed_data or 'expiry_year' in form.changed_data:
             reasons = accounts.get_reasons_account_is_violating_delinquent_criteria(
                 obj.account.id
