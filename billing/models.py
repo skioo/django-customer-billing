5d07b83f0d250d9f150bf500f07c42b2e1788d39
Alberto Pou <albertopouquiros@gmail.com>
2021-01-19 09:24:51 +0100

    Fix comment

--- billing/models.py
+++ billing/models.py
@@ -86,8 +86,8 @@ class Account(Model):
     owner = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='billing_account', on_delete=PROTECT)
     currency = CurrencyField(db_index=True)
     status = FSMField(max_length=20, choices=STATUS_CHOICES, default=OPEN, db_index=True)
-    # An account it is marked as delinquent when it is registered. Then when the user
-    # registers a valid credit card it is marked as compliant.
+    # An account is marked as delinquent when it is registered then when the user
+    # registers a valid credit card it will be marked as compliant
     delinquent = models.BooleanField(default=True, db_index=True)
 
     objects = AccountQuerySet.as_manager()
