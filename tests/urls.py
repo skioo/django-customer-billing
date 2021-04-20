370e7336ed9fb6ff87271cb62c813cfd7cd7b69f
Alberto Pou <bertini36@users.noreply.github.com>
2021-04-20 12:38:15 +0200

    Django 3 and Python 3.8 support (#17)
    * Update setup
    
    * Update tox and travis configs
    
    * Update django stuff
    
    * Merge and fix
    
    * Update package version

--- tests/urls.py
+++ tests/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r'^billing/', include('billing.urls')),
-    path(r'^admin/', admin.site.urls),
+    path(r'billing/', include('billing.urls')),
+    path(r'admin/', admin.site.urls),
 ]
