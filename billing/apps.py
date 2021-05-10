f8ff35dce540d6c14a5effe208744328a25a0bc2
Alberto Pou <albertopouquiros@gmail.com>
2021-05-10 11:37:36 +0200

    Add default_auto_field to app config

--- billing/apps.py
+++ billing/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class BillingConfig(AppConfig):
     name = 'billing'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         # noinspection PyUnresolvedReferences
